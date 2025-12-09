cb3daaee4d125b1d94dacd5122ecc1c67d7671fa
Keshav Agrawal <keshavagrawalwork@gmail.com>
2025-12-09 17:30:03 +0530

    CORS Error Solve

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ success: true, message: "'Keshav Agrawal's Server Is Running" })
